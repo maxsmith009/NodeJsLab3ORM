daa2d0f9195a2bbd869e671d6d65a70e669aa780
Maksim Zalutski <max03246@gmail.com>
2019-05-28 18:33:39 +0300

    dont force sync

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import {sequelize} from './sequelize';
 const port =  3000;
 
 (async () => {
-    await sequelize.sync({force: true});
+    await sequelize.sync({force: false});
 
     createServer(app)
         .listen(
